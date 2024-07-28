1717b4d6b96ebb44bba4bcc49806382f9fda5f31
Satvik Chandra <satvikchandra100@gmail.com>
2024-07-28 20:14:26 +0530

    add auth pages

---
+++ frontend/app/auth/login/page.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <main className="flex w-screen h-screen flex-col items-center bg-gradient-to-b from-slate-50 to-violet-400">
+      <div className="flex flex-col gap-4 bg-white border-2 p-12 mt-24 rounded-xl">
+        <span className="text-4xl font-medium px-8 mb-2">
+          Welcome to <span className="text-purple-900">Workflo</span>!
+        </span>
+        <Input
+          placeholder="Your email"
+          className="bg-zinc-100 focus:border-none"
+        />
+        <Input
+          placeholder="Password"
+          className="bg-zinc-100 focus:border-none"
+        />
+        <Button className="bg-violet-400 shadow-xl hover:bg-violet-500">
+          Login
+        </Button>
+        <span className="m-auto text-zinc-500">
+          Don't have an account? Create a{" "}
+          <Link href={"/"} className="text-blue-600">
+            new account
+          </Link>
+          .
+        </span>
+      </div>
+    </main>
+  );
+}
---
+++ frontend/app/auth/signup/page.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <main className="flex w-screen h-screen flex-col items-center bg-gradient-to-b from-slate-50 to-violet-400">
+      <div className="flex flex-col gap-4 bg-white border-2 p-12 mt-24 rounded-xl">
+        <span className="text-4xl font-medium px-8 mb-2">
+          Welcome to <span className="text-purple-900">Workflo</span>!
+        </span>
+        <Input
+          type="text"
+          placeholder="Full name"
+          className="bg-zinc-100 focus:border-none"
+        />
+        <Input
+          placeholder="Your email"
+          className="bg-zinc-100 focus:border-none"
+        />
+        <Input
+          placeholder="Password"
+          className="bg-zinc-100 focus:border-none"
+        />
+        <Button className="bg-violet-400 shadow-xl hover:bg-violet-500">
+          Sign up
+        </Button>
+        <span className="m-auto text-zinc-500">
+          Already have an account?{" "}
+          <Link href={"/"} className="text-blue-600">
+            Log in
+          </Link>
+          .
+        </span>
+      </div>
+    </main>
+  );
+}
