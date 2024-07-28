fab6e5085db1b45e2d4ed7a87631dfd83353279f
Satvik Chandra <satvikchandra100@gmail.com>
2024-07-28 22:49:30 +0530

    add navbar items

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,57 +1,11 @@
-import Image from "next/image";
-
-type HeaderItemType = {
-  id: number;
-  image: string;
-  title: string;
-  desc: string;
-};
-
-const headerItems: HeaderItemType[] = [
-  {
-    id: 1,
-    image: "one.png",
-    title: "Introducing tags",
-    desc: "Easily categorize and find your notes by adding tags. Keep your workspace clutter-free and efficient.",
-  },
-  {
-    id: 2,
-    image: "two.png",
-    title: "Share Notes Instantly",
-    desc: "Effortlessly share your notes with others via email or link. Enhance collaboration with quick sharing options.",
-  },
-  {
-    id: 3,
-    image: "three.png",
-    title: "Access Anywhere",
-    desc: "Sync your notes across all devices. Stay productive whether you're on your phone, tablet, or computer.",
-  },
-];
+import HeaderItems from "@/components/HeaderItems";
+import Navbar from "@/components/Navbar";
 
 export default function Dashboard() {
   return (
-    <div className="bg-zinc-100 pt-6 px-4">
-      <span className="text-4xl font-bold">Good morning, Joe!</span>
-      <div className="flex gap-4 mt-6">
-        {headerItems.map((item) => {
-          return (
-            <div className="flex bg-white rounded-md p-6" key={item.id}>
-              <Image
-                src={"/" + item.image}
-                width={70}
-                height={60}
-                alt={item.image}
-              />
-              <div className="flex flex-col px-6 gap-1">
-                <span className="text-zinc-500 font-semibold">
-                  {item.title}
-                </span>
-                <span className="text-zinc-500 text-sm">{item.desc}</span>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+    <div className="flex flex-col gap-4 bg-zinc-100 pt-6 px-4">
+      <HeaderItems />
+      <Navbar />
     </div>
   );
 }
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import {
+  Calendar,
+  CirclePlus,
+  Filter,
+  Search,
+  Share2,
+  Sparkles,
+} from "lucide-react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between relative">
+        <Input type="text" placeholder="Search" />
+        <Search className="absolute right-0 mr-2 text-zinc-400" />
+      </div>
+      <div className="flex gap-2">
+        <Button variant={"outline"}>
+          Calender View <Calendar className="ml-2" />
+        </Button>
+        <Button variant={"outline"}>
+          Automation <Sparkles className="ml-2" />
+        </Button>
+        <Button variant={"outline"}>
+          Filter <Filter className="ml-2" />
+        </Button>
+        <Button variant={"outline"}>
+          Share <Share2 className="ml-2" />
+        </Button>
+        <Button className="bg-violet-800 hover:bg-violet-900 shadow-lg">
+          Create new
+          <CirclePlus className="w-5 h-5 ml-2" />
+        </Button>
+      </div>
+    </div>
+  );
+}
