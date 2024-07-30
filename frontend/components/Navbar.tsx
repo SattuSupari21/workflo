302992047a3847d4c668c87c6f772152e689a740
Satvik Chandra <satvikchandra100@gmail.com>
2024-07-30 11:37:21 +0530

    done: navbar create task dialog

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -4,6 +4,7 @@ import {
   Filter,
   Loader,
   Pencil,
+  Plus,
   Search,
   Share2,
   Sparkles,
@@ -12,11 +13,16 @@ import {
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
@@ -55,27 +61,89 @@ function CreateNewTaskDialog() {
         <CirclePlus className="w-5 h-5 ml-2" />
       </DialogTrigger>
       <DialogContent className="flex flex-col gap-8 text-zinc-700">
-        <Input
+        <input
           type="text"
           placeholder="Title"
-          className="text-4xl p-0 border-0 mt-6"
+          className="text-4xl p-0 border-0 mt-6 focus:outline-none font-semibold text-zinc-500"
         />
-        <div className="flex gap-4 items-center">
-          <Loader className="w-5 h-5 " />
-          Status
-        </div>
-        <div className="flex gap-4 items-center">
-          <TriangleAlert />
-          Priority
-        </div>
-        <div className="flex gap-4 items-center">
-          <Calendar />
-          Deadline
-        </div>
-        <div className="flex gap-4 items-center">
-          <Pencil />
-          Desciption
-        </div>
+        <div className="text-sm flex flex-col gap-3 text-zinc-500">
+
+          <div className="flex justify-between">
+            <div className="flex gap-5 items-center">
+              <Loader className="w-5 h-5" />
+              Status
+            </div>
+            <Select>
+              <SelectTrigger className="border-0 max-w-[300px]">
+                <SelectValue placeholder="Not selected" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="To do">To do</SelectItem>
+                <SelectItem value="In progress">In progress</SelectItem>
+                <SelectItem value="Under review">Under review</SelectItem>
+                <SelectItem value="Finished">Finished</SelectItem>
+              </SelectContent>
+          </Select>
+          </div>
+
+          <div className="flex justify-between">
+            <div className="flex gap-5 items-center">
+              <TriangleAlert className="w-5 h-5" />
+              Priority
+            </div>
+            <Select>
+              <SelectTrigger className="border-0 max-w-[300px]">
+                <SelectValue placeholder="Not selected" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="To do">To do</SelectItem>
+                <SelectItem value="In progress">In progress</SelectItem>
+                <SelectItem value="Under review">Under review</SelectItem>
+                <SelectItem value="Finished">Finished</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="flex justify-between">
+            <div className="flex gap-5 items-center">
+            <Calendar className="w-5 h-5" />
+            Deadline
+            </div>
+            <Select>
+              <SelectTrigger className="border-0 max-w-[300px]">
+                <SelectValue placeholder="Not selected" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="To do">To do</SelectItem>
+                <SelectItem value="In progress">In progress</SelectItem>
+                <SelectItem value="Under review">Under review</SelectItem>
+                <SelectItem value="Finished">Finished</SelectItem>
+              </SelectContent>
+          </Select>
+          </div>
+
+          <div className="flex justify-between">
+            <div className="flex gap-5 items-center">
+              <Pencil className="w-5 h-5" />
+              Desciption
+            </div>
+            <input
+              type="text"
+              placeholder="Not selected"
+              className="min-w-[300px] p-3 border-0 focus:outline-none text-zinc-500"
+            />
+          </div>
+
+          <div className="flex gap-5 items-center text-zinc-900 mt-2">
+            <Plus className="w-5 h-5" />
+            <div>Add custom property</div>
+          </div>
+
+          <div className="border-b mt-6 mb-5"></div>
+          
+          <div className="pb-[300px]">Start writing, or drag your own files here.</div>
+          
+          </div>
       </DialogContent>
     </Dialog>
   );
